e2effbd6841d612de9b2e5fd9f3fc28349d57181
김근수 <guksuukim@naver.com>
2023-10-02 17:04:50 +0900

    web programing

---
+++ app/api/courses/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+import Courses from '@/components/Courses'
+const CoursesPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4">Courses </h1>
+            <Courses />
+        </div>
+    )
+}
+export default CoursesPage
